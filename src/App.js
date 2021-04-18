c28b0c85ca5b150c8650c156660a6af0c1a86b2f
Ghassen <ghassen.boughzala@esprit.tn>
2021-04-18 00:05:35 +0000

    Fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Router, Switch, Route, Redirect } from "react-router-dom";
-import UseAnimations from 'react-useanimations';
 
 //navbars to change based on auth
 import NavbarCandidate from "./components/Navbars/CandidateNavbar";
